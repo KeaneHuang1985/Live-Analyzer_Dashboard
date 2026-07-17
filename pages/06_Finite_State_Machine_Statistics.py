255b023a6d24ca7b3f7f4ed4a49981918916fd2d
Ken Huang <ken.huang@ubiik.com>
2026-07-17 14:57:01 +0800

    create the render_toa_statistics

---
+++ pages/06_Finite_State_Machine_Statistics.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+from core.data_center import get_global_data
+from core.render_helper import render_if_changed
+from modules.GCT_Finite_State_Machine import render_fsm_statistics
+from modules.GCT_Finite_State_Machine import render_toa_statistics
+from modules.phy_rx_req import render_phy_rx_req
+
+
+
+st.set_page_config(page_title="03_Finite_State_Machine_Statistics",layout="wide")
+st.title("Finite State Machine Statistics")
+
+
+@st.fragment(run_every="5s")
+def fragment_fsm_statistics():
+    global_data = get_global_data()
+
+    df_raw = pd.DataFrame(global_data.get_fsm_list())
+    
+    if render_if_changed(df_raw,"last_fsm_list"):
+        render_fsm_statistics(df_raw)
+    else:
+        print("Did not need update page 04_Finite_State_Machine_Statistics")
+
+
+@st.fragment(run_every="5s")
+def fragment_phy_rx_req():
+    global_data = get_global_data()
+    phy_rx_req = global_data.get_phy_rx_req()
+    if len(phy_rx_req) > 0:
+        render_phy_rx_req(phy_rx_req)
+    else:
+        print("Did not need update page 04_Finite_State_Machine_Statistics")
+
+@st.fragment(run_every="8s")
+def fragment_toa_statistics():
+    global_data = get_global_data()
+    df_raw = pd.DataFrame(global_data.get_toa_sample())
+    if render_if_changed(df_raw,"last_toa_sample"):
+        render_toa_statistics(df_raw)
+    else:
+        print("Did not need update page 04_Finite_State_Machine_Statistics")
+
+fragment_fsm_statistics()
+fragment_phy_rx_req()
